147938a5f9d54523f65e4c27352ea04bdbb37a34
Kosiso29 <kafoenyi@gmail.com>
2020-09-12 22:19:02 +0100

    Updates

---
+++ precache-manifest.e515e2820a02ee959db98cbacf9927a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bb56f22be8fdc25ee83f864541accc",
+    "url": "/projectreact/index.html"
+  },
+  {
+    "revision": "ed60f62ac10769baaf2e",
+    "url": "/projectreact/static/css/2.23fdb708.chunk.css"
+  },
+  {
+    "revision": "8a0b58149996f031e99f",
+    "url": "/projectreact/static/css/main.2ba868c9.chunk.css"
+  },
+  {
+    "revision": "ed60f62ac10769baaf2e",
+    "url": "/projectreact/static/js/2.16bef95b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projectreact/static/js/2.16bef95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0b58149996f031e99f",
+    "url": "/projectreact/static/js/main.895581d9.chunk.js"
+  },
+  {
+    "revision": "3cbbfbd7b53fd3007e23",
+    "url": "/projectreact/static/js/runtime-main.04b957d6.js"
+  }
+]);
